cc99c9539f521c731cb5378dda55fbaf88264198
KrisWaw <44495365+KrisWaw@users.noreply.github.com>
2020-10-10 20:44:34 +0200

    add view with projects of a chosen building and get-by-building.js to fetch them

---
+++ js/projects/get-by-building.js
@@ -0,0 +1,32 @@
+let query = window.location.search;
+let urlParam = new URLSearchParams(query);
+let buildingId = urlParam.get('id');
+
+const url = `${SERVER_NAME}/buildings/${buildingId}/projects`
+
+fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+
+        let output = '';
+
+        data.forEach(project => {
+            output += `
+        <tr>
+          <td>${project.id}</td>
+          <td>${project.date}</td>
+          <td>${project.floor}</td>
+          <td>${project.mpk}</td>
+          <td>${project.project_num}</td>
+          <td>${project.short_description}</td>
+          <td>${project.tenant}</td>
+          <td>${project.service}</td>
+        </tr>
+        `;
+        });
+
+        document.querySelector('#projects-list').innerHTML = output;
+});
+
+const buildingName = sessionStorage.getItem("buildingName");
+document.querySelector('#building-name').textContent = `Projekty Budynku ${buildingName}`;
