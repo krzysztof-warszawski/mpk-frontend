783a6767a8e3075bdf7f1c26dcd2af1ab693bdd7
KrisWaw <44495365+KrisWaw@users.noreply.github.com>
2020-10-17 00:41:51 +0200

    add createDirectories fn

--- js/projects/get-all.js
+++ js/projects/get-all.js
@@ -14,9 +14,42 @@ fetch(`${SERVER_NAME}/projects`)
       <td>${project.short_description}</td>
       <td>${project.tenant}</td>
       <td>${project.service}</td>
-    </tr>
     `;
+
+    if (project.service === 'Gwarancja') {
+      output += `
+          <td></td>
+        </tr>
+      `;
+    } else {
+      output += `
+          <td><button class="btn btn-warning btn-sm" value="${project.id}" id="id" 
+          onclick="createDirectories(value);">Generuj</button></td>
+        </tr>
+      `;
+    }
   });
 
   document.querySelector('#projects-list').innerHTML = output;
 });
+
+function createDirectories(id) {
+  fetch(`${SERVER_NAME}/directories/${id}`,{
+    method: 'POST',
+    headers: {
+      "Accept" : "application/json, text/plain, */*",
+      "Content-type": "application/json; charset=UTF-8",
+      "Content-Length" : "0"
+    },
+    body: ""
+  }).then(response => {
+    if (response.status === 200) {
+      response.json()
+          .then(data => {
+            console.log(data['info']);
+          })
+    }
+  }).catch((error) => {
+      console.error('Error:', error);
+  });
+}
